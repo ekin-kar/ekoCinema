df0b107e96ad3f6a969219f95aba15b3706514ba
EkinKar <kar_ekin@outlook.com>
2024-03-22 23:07:06 +0300

    fixed nextconfig deprecated error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",
-      "pegem.net",
-      "m.media-amazon.com",
-      "cdn.europosters.eu",
-      "rukminim2.flixcart.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "pegem.net",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.europosters.eu",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "rukminim2.flixcart.com",
+        pathname: "**",
+      },
     ],
   },
 };
