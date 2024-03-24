4f8f126dfa3ec5696228da72feb43d5cda6f3ea4
EkinKar <kar_ekin@outlook.com>
2024-03-24 16:09:24 +0300

    edited landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,13 +5,15 @@ import styles from "./page.module.css";
 const page = async () => {
   const cities = await getCities();
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Welcome to EkoCinema!</h2>
-      <p className={styles.text}>Start by selecting your city</p>
-      <div className={styles.cityContainer}>
-        {cities.map((city, index) => (
-          <CityCard city={city} key={index} />
-        ))}
+    <div className={styles.bg}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Welcome to EkoCinema!</h2>
+        <p className={styles.text}>Start by selecting your city</p>
+        <div className={styles.cityContainer}>
+          {cities.map((city, index) => (
+            <CityCard city={city} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
